8669cad8bdc9d639f1f71bc830f151be01fc381b
maia <maiazeldaverdier@gmail.com>
2021-10-26 14:13:14 +0200

    project creation

---
+++ pharmacy-backend/src/test/java/com/grp3/pharmacybackend/PharmacyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grp3.pharmacybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
