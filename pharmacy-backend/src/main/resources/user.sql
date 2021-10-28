d56cf788a0039cf28bbe2e14f9f64f1b64144a6f
maia <maiazeldaverdier@gmail.com>
2021-10-28 09:53:17 +0200

    add script sql databse + userGrp3

---
+++ pharmacy-backend/src/main/resources/user.sql
@@ -0,0 +1,13 @@
+-- Role: "userGrp3"
+-- DROP ROLE "userGrp3";
+
+CREATE ROLE "userGrp3" WITH
+  LOGIN
+  SUPERUSER
+  INHERIT
+  NOCREATEDB
+  NOCREATEROLE
+  NOREPLICATION
+  ENCRYPTED PASSWORD 'md557f860d5803739d4b77f002038ec9159';
+
+  // password = password //
