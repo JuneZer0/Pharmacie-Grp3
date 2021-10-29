387c3f52651a382019e55b5a57780ccf1db8c024
maia <maiazeldaverdier@gmail.com>
2021-10-29 12:06:19 +0200

    add findArticlebyId in ArticleServiceImpl

--- pharmacy-backend/src/main/java/com/grp3/pharmacybackend/business/Helpers/ModelMapper.java
+++ pharmacy-backend/src/main/java/com/grp3/pharmacybackend/business/Helpers/ModelMapper.java
@@ -1,6 +1,8 @@
 package com.grp3.pharmacybackend.business.Helpers;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 import com.grp3.pharmacybackend.persistance.entities.Article;
 import com.grp3.pharmacybackend.presentation.model.ArticleDto;
 
@@ -12,7 +14,7 @@ public class ModelMapper {
       
          // map un article ---> articleDto
         
-        public ArticleDto mapToArticleDto(Article article) {
+         public ArticleDto mapToArticleDto(Article article) {
             ArticleDto articleDto = new ArticleDto();
             if (article == null) {
                 return null;
@@ -41,8 +43,9 @@ public class ModelMapper {
         }
     
         
+  
           //map une liste d'objets Do ---> liste d'objets Dto
-        public List<ArticleDto> mapToListArticlesDto(final List<Article> listArticlesDo) {
+          public List<ArticleDto> mapToListArticlesDto(final List<Article> listArticlesDo) {
             System.out.println(listArticlesDo);
             List<ArticleDto> listArticlesDto = new ArrayList<>();
             for (Article articleDo : listArticlesDo) {
