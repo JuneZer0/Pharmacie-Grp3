c5a43cfe90fccff15bce5b0c5e28ac9c1e3b69c0
Kraken-dsk <c.peter.6742@gmail.com>
2021-10-29 14:46:02 +0200

    repository

--- pharmacy-backend/src/main/java/com/grp3/pharmacybackend/persistance/dao/interfaces/IArticleDao.java
+++ pharmacy-backend/src/main/java/com/grp3/pharmacybackend/persistance/dao/interfaces/IArticleDao.java
@@ -2,6 +2,9 @@ package com.grp3.pharmacybackend.persistance.dao.interfaces;
 
 import com.grp3.pharmacybackend.persistance.entities.Article;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface IArticleDao extends IGenericDao<Article>{
     
 }
