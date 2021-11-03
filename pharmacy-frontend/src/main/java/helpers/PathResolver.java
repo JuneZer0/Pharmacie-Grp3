4582324f334f676a393721b172f29f94bfb7c47f
Caroline <c.peter.6742@gmail.com>
2021-11-03 12:46:49 +0100

    path correction

--- pharmacy-frontend/src/main/java/helpers/PathResolver.java
+++ pharmacy-frontend/src/main/java/helpers/PathResolver.java
@@ -18,8 +18,8 @@ public class PathResolver {
 
 
     //JSP Paths
-    public static final String JSP_MENU ="./webapp/WEB-INF/header.jsp";
-    public static final String JSP_FOOTER ="./webapp/WEB-INF/footer.jsp";
+    public static final String JSP_MENU ="/WEB-INF/header.jsp";
+    public static final String JSP_FOOTER ="/WEB-INF/footer.jsp";
     public static final String JSP_HOME="";
     public static final String JSP_PRODUCT="";
     public static final String JSP_FORM="";
